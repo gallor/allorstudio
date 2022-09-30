a978dae138222b45aea5ed3976f77c4350809f60
Grant Allor <gallor@drwholdings.com>
2022-09-30 10:12:27 -0500

    About page text and new pictures

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,8 +10,8 @@ const AboutPage = (props) => (
     <PageLayout>
       <Seo title="About" />
       <AboutImage />
-      <p>Jennifer Allor has established herself as a pianist and vocal coach in the Chicago area and is currently entering her second year as a Young Artist with Chicago Opera Theater. During the 2021-2022 season, she worked on productions of Carmen, Becoming Santa Claus (Adamo), The Beekeeper (Wang Lu), and the world premiere of Quamino’s Map
-(Errollyn Wallen) with COT. She also appeared with COT Young Artist singers in recitals and performances. For the summer of 2022, she joined Prague Summer Nights as apprentice coach for their production of Don Giovanni and served as pianist and coach for Soo Opera Theatre’s production of La Bohème. She has appeared as soloist with multiple orchestras including the Charlotte and Raleigh Symphonies. She holds a Master of Music degree in piano performance from the Cleveland Institute of Music and a Bachelor of Music degree in piano performance from the University of North Carolina at Chapel Hill.</p>
+      <p>Jennifer Allor has established herself as a pianist and vocal coach in the Chicago area and is currently entering her second year in the Chicago Opera Theater/Chicago College of the Performing Arts at Roosevelt University Professional Diploma and Young Artist Program. During the 2021-2022 season, she worked on productions of <i>Carmen</i>, <i>Becoming Santa Claus</i> (Adamo), <i>The Beekeeper</i> (Wang Lu), and the world premiere of <i>Quamino’s Map</i>
+(Errollyn Wallen) with COT. She also appeared with COT Young Artist singers in recitals and performances. For the summer of 2022, she joined Prague Summer Nights as apprentice coach for their production of <i>Don Giovanni</i> and served as pianist and coach for Soo Opera Theatre’s production of <i>La Bohème</i>. She has appeared as soloist with multiple orchestras including the Charlotte and Raleigh Symphonies. She holds a Master of Music degree in piano performance from the Cleveland Institute of Music and a Bachelor of Music degree in piano performance from the University of North Carolina at Chapel Hill.</p>
     </PageLayout>
   </Layout>
 )
